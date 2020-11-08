9dbc911d820af8230935f13990796960ef72e88b
liujian361000 <350987560@qq.com>
2020-11-08 12:05:13 +0800

    冲突修改测试1

--- cloudconfigserver-eureka/src/main/java/com/liu/cloudconfigservereureka/controllers/CaseController.java
+++ cloudconfigserver-eureka/src/main/java/com/liu/cloudconfigservereureka/controllers/CaseController.java
@@ -3,18 +3,19 @@ package com.liu.cloudconfigservereureka.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class CaseController {
-    
-    String name;
-    
-    public void sayHi(){
-        
-    }
 
     @RequestMapping("/hi")
     public String hi(){
-        name="liujian";
+        List<String> list=new ArrayList<>();
+        list.add("张三");
+        list.add("里斯");
+        list.add("王武");
+        list.add("赵六");
         return "hi";
     }
 
