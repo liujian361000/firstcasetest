530914d65809f857b3418248b8cac0cee800e08a
liujian361000 <73952235+liujian361000@users.noreply.github.com>
2020-11-08 11:55:04 +0800

    Update CaseController.java
    测试冲突

--- cloudconfigserver-eureka/src/main/java/com/liu/cloudconfigservereureka/controllers/CaseController.java
+++ cloudconfigserver-eureka/src/main/java/com/liu/cloudconfigservereureka/controllers/CaseController.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CaseController {
+    
+    String name;
+    
+    public void sayHi(){
+        
+    }
 
     @RequestMapping("/hi")
     public String hi(){
+        name="liujian";
         return "hi";
     }
 
